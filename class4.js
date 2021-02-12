18e6758d5d8d0eaf7678e4383847dc03afdd44db
dsrikanth776 <75516370+dsrikanth776@users.noreply.github.com>
2021-02-12 16:49:44 +0530

    Add files via upload

---
+++ class4.js
@@ -0,0 +1,34 @@
+// object          object
+// kishore         srikanth
+// var               data
+// goodmorning      good morning
+//hadubreakfast      yes
+//complete           whtaboutu
+//whatrudoing        leaclass
+//howmuchfee          800
+
+
+function course(){
+    var goodmorning="goodmorning"
+    var hadubreakfast="yes"
+    var whataboutu="complete"
+    var whatrudoing="leaclass"
+    var howmuchfee=800
+    If(fee>600)
+    {
+        document.write("code stop")
+        return
+    }
+    
+    
+    var joincourse="javascript"
+    var emi=100
+    console.log("i have joined")
+    return
+    
+
+
+
+}
+course()
+
